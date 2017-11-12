e48bdbf49d4d260fee9d26a44890e9904f3cead6
Ahmed.Saber <smartic@gmail.com>
2017-11-12 11:30:43 +0200

    Adding the navigation

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,20 +1,26 @@
 // VARS
 let markers = [{
-    lat: 30.1061532,
-    lng: 31.3309798
+    'name':'1',
+    'lat': 30.1061532,
+    'lng': 31.3309798
 }, {
-    lat: 30.105927,
-    lng: 31.3229273
+    'name':'s',
+    'lat': 30.105927,
+    'lng': 31.3229273
 }, {
-    lat: 30.0925206,
-    lng: 31.3247189
+    'name':'d',
+    'lat': 30.0925206,
+    'lng': 31.3247189
 }, {
-    lat: 30.0925265,
-    lng: 31.3338122
+    'name':'f',
+    'lat': 30.0925265,
+    'lng': 31.3338122
 }, {
-    lat: 30.0906328,
-    lng: 31.3332114
+    'name':'g',
+    'lat': 30.0906328,
+    'lng': 31.3332114
 }];
+
 // GOOGLE MAPS API CALLBACK
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -34,11 +40,19 @@ function initMap() {
 
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
+    // FOR SEARCH
     this.query = ko.observable();
     this.query.subscribe(function () {
 
     });
+    // MARKERS
+    this.markers = ko.observableArray(markers);
 }
+
+// Menu toggle btn
+$('#menuToggleBtn').click(function () {
+    $('.menu-holder').toggleClass('open');
+});
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());

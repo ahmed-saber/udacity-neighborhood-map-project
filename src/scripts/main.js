471c0dc0f31a9c96cdac8bc741ac9792e2d58b06
Ahmed.Saber <smartic@gmail.com>
2017-11-12 15:33:19 +0200

    Adding new locations and adding events

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,11 @@ const CLIENT_ID = '4DKV23J31PTRWEMCJUER3YAE3R4IU5LDZ0X3112KJI50JFRL';
 const CLIENT_SECRET = 'EYAVLRDXWHU22DCQEHHJCWGSFHGRX5TW3MKEP4DNJTPN00HR';
 const apiURL = `https://api.foursquare.com/v2/venues/search?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20160118`;
 let locations = [{
-    lat: 30.1061532,
-    lng: 31.3309798
+    lat: 30.08143,
+    lng: 31.3434445
 }, {
-    lat: 30.105927,
-    lng: 31.3229273
+    lat: 30.0916028,
+    lng: 31.3187227
 }, {
     lat: 30.0925206,
     lng: 31.3247189
@@ -23,33 +23,41 @@ let locations = [{
 
 // Adds a marker to the map and push to the array.
 function addMarker(location) {
-    return $.ajax({
-        url: apiURL,
-        data: {
-            ll: `${location.lat},${location.lng}`
-        },
-        success: function (data) {
-            // VARS
-            var info = data.response.venues[0];
-            var marker = new google.maps.Marker({
-                position: location,
-                map: map
-            });
-            location.name = info.name;
-            location.marker = marker;
-            // ADD CLICK EVENT
-            location.menuLinkAction = function () {
-                
-            };
-            markers.push(location);
-        }
+    var info = location.info.response.venues[0];
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
     });
+    location.name = info.name;
+    location.marker = marker;
+    location.marker.addListener('click', function () {
+        // CLOSE OTHER WINDOWS
+        locations.forEach((loc)=>{loc.infoWindow.close();});
+        location.infoWindow.open(map, this);
+        location.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function () {
+            location.marker.setAnimation(null);
+        }, 2090);
+    });
+    // ADD CLICK EVENT
+    location.infoWindow = new google.maps.InfoWindow({
+        content: `
+            <p>Name: ${info.name}</p>
+            <p>Category: ${info.categories[0].name}</p>
+            <p>Country: ${info.location.country}</p>
+            <p>State: ${info.location.state}</p>
+        `
+    });
+    location.menuLinkAction = function () {
+        google.maps.event.trigger(location.marker, 'click');
+    };
+    markers.push(location);
 }
 
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].marker.setMap(map);
+    for (var i = 0; i < markers().length; i++) {
+        markers()[i].marker.setMap(map);
     }
 }
 
@@ -64,7 +72,11 @@ function updateLists(locations) {
     var bounds = new google.maps.LatLngBounds();
     // LOOP ON MARKERS
     locations.forEach(function (location) {
-        addMarker(location);
+        if(location.name){
+            addMarker(location);
+        }else{
+            callApi(location);
+        }
         bounds.extend(location);
     }, this);
     // ZOOM ON MARKERS ONLY
@@ -80,6 +92,18 @@ function AppViewModel() {
     this.query.subscribe(function () {
         // CLEAR MARKERS
         clearMarkers();
+        // FILTER BY LOCATION NAME
+        var newlocations = locations.filter(function(location){
+            // VARS
+            let query = self.query().toLowerCase();
+            let result = (location.name.toLowerCase().search(query) >= 0);
+            if(result){
+                addMarker(location);
+            }
+            return result;
+        });
+        // UPDATE THE MENU
+        markers(newlocations);
     });
     // MARKERS
     this.markers = markers;
@@ -94,6 +118,21 @@ function initMap() {
     updateLists(locations);
     // Activates knockout.js
     ko.applyBindings(new AppViewModel());
+}
+
+// GET LOCATION DETAILS
+function callApi(location){
+    $.ajax({
+        url: apiURL,
+        data: {
+            ll: `${location.lat},${location.lng}`
+        },
+        success: function (data) {
+            // VARS
+            location.info = data;
+            addMarker(location);
+        }
+    });
 }
 
 // Menu toggle btn
